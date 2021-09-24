5db30a28c457c3add5ed386abf72a55dd0a7186c
ngohuyhieu <91303660+ngohuyhieu@users.noreply.github.com>
2021-09-24 22:55:09 +0700

    Add files via upload

---
+++ BuoiHocDauTien.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("dem khong trang dem buon vo tan\n");
+	printf("ben khong do lo buoc sang ngang\n");
+	printf("doi khong yeu la mot doa hoa tan\n");
+	printf("yeu khong thanh la muon van dau kho\n");
+	printf("nam thang troi di nam thang buon\n");
+	printf("nhin lai nam thang le cang tuon\n");
+	printf("nguoi ay vui ve noi nao vay\n");
+	printf("co biet noi day mot nguoi buon\n");
+	return 0;
+}
